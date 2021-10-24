4e3be36f8e1c618e132f387e72cbff174273c438
siki <musyajuaayush@gmail.com>
2021-10-24 09:09:47 +0545

    initialized home directory

---
+++ src/components/Home.tsx
@@ -0,0 +1,33 @@
+//import { Icon } from "@iconify/react";
+import { FaLinkedinIn, FaFacebookF, FaTwitter, FaGithub } from "react-icons/fa";
+
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="home__intro">
+                Hey,<br></br>I'm Aayush musyaju
+            </div>
+            <div className="home__icons">
+                <a href="/" className="home__facebook">
+                    <FaFacebookF className="icon_test" />
+                </a>
+                <a href="/" className="home__github">
+                    <FaGithub className="icon_test" />
+                </a>
+                <a href="/" className="home__twitter">
+                    <FaTwitter className="icon_test" />
+                </a>
+                <a href="/" className="home__linkedin">
+                    <FaLinkedinIn className="icon_test" />
+                </a>
+            </div>
+            <div className="home__about">
+                I'm a Product Designer and visual thinker passionate about
+                humanizing tech experiences and creating thoughtful products for
+                community and well being. Best paired with this playlist.
+            </div>
+        </div>
+    );
+};
+
+export default Home;
