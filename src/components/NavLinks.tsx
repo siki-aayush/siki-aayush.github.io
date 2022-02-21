f2e748058580ec5de9e336ae768c96a4a1ff6522
siki <musyajuaayush@gmail.com>
2022-02-21 22:47:12 +0545

    changes indent spaces from 4 to 2

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -3,51 +3,47 @@ import { Link } from "react-router-dom";
 import { Page } from "../Interfaces";
 
 interface navProps {
-    currentPage: Page;
-    onPageChange: () => void;
+  currentPage: Page;
+  onPageChange: () => void;
 }
 
 const NavLinks = ({ currentPage, onPageChange }: navProps) => {
-    return (
-        <>
-            <div className="nav__element">
-                <Link
-                    to="/"
-                    className="nav__element__home"
-                    onClick={onPageChange}
-                >
-                    Home
-                    {currentPage === Page.home && (
-                        <div className="nav__element__highlight" />
-                    )}
-                </Link>
-            </div>
-            <div className="nav__element">
-                <Link
-                    to="/about"
-                    className="nav__element__about"
-                    onClick={onPageChange}
-                >
-                    About
-                    {currentPage === Page.about && (
-                        <div className="nav__element__highlight" />
-                    )}
-                </Link>
-            </div>
-            <div className="nav__element">
-                <Link
-                    to="/projects"
-                    className="nav__element__projects"
-                    onClick={onPageChange}
-                >
-                    Projects
-                    {currentPage === Page.projects && (
-                        <div className="nav__element__highlight" />
-                    )}
-                </Link>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="nav__element">
+        <Link to="/" className="nav__element__home" onClick={onPageChange}>
+          Home
+          {currentPage === Page.home && (
+            <div className="nav__element__highlight" />
+          )}
+        </Link>
+      </div>
+      <div className="nav__element">
+        <Link
+          to="/about"
+          className="nav__element__about"
+          onClick={onPageChange}
+        >
+          About
+          {currentPage === Page.about && (
+            <div className="nav__element__highlight" />
+          )}
+        </Link>
+      </div>
+      <div className="nav__element">
+        <Link
+          to="/projects"
+          className="nav__element__projects"
+          onClick={onPageChange}
+        >
+          Projects
+          {currentPage === Page.projects && (
+            <div className="nav__element__highlight" />
+          )}
+        </Link>
+      </div>
+    </>
+  );
 };
 
 export default NavLinks;
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -3,38 +3,38 @@ import insertIcon from "../programmingIcon";
 import { RiGitRepositoryLine, RiComputerLine } from "react-icons/ri";
 
 interface projectCardProps {
-    repo: repository;
+  repo: repository;
 }
 
 const ProjectCard = ({ repo }: projectCardProps) => {
-    repo.name = repo.name.replaceAll("_", " ");
-    repo.name = repo.name.replaceAll("-", " ");
-    repo.name = repo.name.charAt(0).toUpperCase() + repo.name.slice(1);
-    return (
-        <div className="projectCard">
-            <div className="projectCard__title">{repo.name}</div>
-            <div className="projectCard__desc">{repo.description}</div>
-            <div className="projectCard__lang">
-                {repo.languages.map((lang) => insertIcon(lang.toLowerCase()))}
-                {repo.fork ? insertIcon("fork") : null}
-            </div>
-            <div className="projectCard__btn flex flex-ac">
-                <button
-                    className="projectCard__btn__repo flex flex-ac flex-c"
-                    onClick={() => window.open(repo.url)}
-                >
-                    <RiGitRepositoryLine /> Repository
-                </button>
-                <button
-                    className="projectCard__btn__demo flex flex-ac flex-c "
-                    onClick={() => window.open(repo.homepage)}
-                    disabled={!repo.has_pages}
-                >
-                    <RiComputerLine /> Website
-                </button>
-            </div>
-        </div>
-    );
+  repo.name = repo.name.replaceAll("_", " ");
+  repo.name = repo.name.replaceAll("-", " ");
+  repo.name = repo.name.charAt(0).toUpperCase() + repo.name.slice(1);
+  return (
+    <div className="projectCard">
+      <div className="projectCard__title">{repo.name}</div>
+      <div className="projectCard__desc">{repo.description}</div>
+      <div className="projectCard__lang">
+        {repo.languages.map((lang) => insertIcon(lang.toLowerCase()))}
+        {repo.fork ? insertIcon("fork") : null}
+      </div>
+      <div className="projectCard__btn flex flex-ac">
+        <button
+          className="projectCard__btn__repo flex flex-ac flex-c"
+          onClick={() => window.open(repo.url)}
+        >
+          <RiGitRepositoryLine /> Repository
+        </button>
+        <button
+          className="projectCard__btn__demo flex flex-ac flex-c "
+          onClick={() => window.open(repo.homepage)}
+          disabled={!repo.has_pages}
+        >
+          <RiComputerLine /> Website
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectCard;
